4434648ed08bd3878506e68d424d833a8ff43d39
Muhammed Hamdy <90095538+hamdy-jr@users.noreply.github.com>
2023-11-28 07:57:06 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,37 @@
-// Create wev service
+// Create web service
+// 1. Get comments by id
+// 2. Get comments by user id
+// 3. Create comment
+// 4. Update comment
+// 5. Delete comment
+// 6. Get all comments
 
-// import module
-
+// Import express
 const express = require("express");
+// Import router
 const router = express.Router();
+// Import model
+const Comment = require("../models/comment");
+// Import controller
 const commentController = require("../controllers/commentController");
 
-// handle request
-router.get("/", commentController.comment_list);
-router.get("/create", commentController.comment_create_get);
-router.post("/create", commentController.comment_create_post);
-router.get("/:id/delete", commentController.comment_delete_get);
-router.post("/:id/delete", commentController.comment_delete_post);
-router.get("/:id/update", commentController.comment_update_get);
-router.post("/:id/update", commentController.comment_update_post);
-router.get("/:id", commentController.comment_detail);
+// Get all comments
+router.get("/", commentController.get_all_comments);
 
+// Get comment by id
+router.get("/:commentId", commentController.get_comment_by_id);
+
+// Get comment by user id
+router.get("/user/:userId", commentController.get_comment_by_user_id);
+
+// Create comment
+router.post("/", commentController.create_comment);
+
+// Update comment
+router.patch("/:commentId", commentController.update_comment);
+
+// Delete comment
+router.delete("/:commentId", commentController.delete_comment);
+
+// Export module
 module.exports = router;
