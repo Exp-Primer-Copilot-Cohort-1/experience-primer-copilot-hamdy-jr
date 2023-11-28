e6eb46b5db63f9b1d8a316a6f288c911e2a4d876
Muhammed Hamdy <90095538+hamdy-jr@users.noreply.github.com>
2023-11-28 07:53:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,10 +1,13 @@
 // Create wev service
 
+// import module
+
 const express = require("express");
 const router = express.Router();
 const commentController = require("../controllers/commentController");
 
-router.get("/list", commentController.comment_list);
+// handle request
+router.get("/", commentController.comment_list);
 router.get("/create", commentController.comment_create_get);
 router.post("/create", commentController.comment_create_post);
 router.get("/:id/delete", commentController.comment_delete_get);
