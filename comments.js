14cfb61803f6323d9493aac0d4cd0880d2cca6c2
Muhammed Hamdy <90095538+hamdy-jr@users.noreply.github.com>
2023-11-28 08:09:30 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,75 +1,18 @@
-// Create Web Service
-// Date: 10/19/2018
-// Programmer: Phil Graham
+// Create web server
 
-// Import Modules
+// import module
 const express = require("express");
 const router = express.Router();
+const CommentController = require("../controllers/comment.controller");
 
-// Import Models
-const Comment = require("../models/comment");
+// Handle Request
+router.get("/", CommentController.index);
+router.get("/detail/:id", CommentController.detail);
+router.get("/create", CommentController.create);
+router.post("/create", CommentController.postCreate);
+router.get("/delete/:id", CommentController.delete);
+router.get("/update/:id", CommentController.update);
+router.post("/update/:id", CommentController.postUpdate);
+router.get("/delete/:id", CommentController.delete);
 
-// Web Service
-// Get all comments
-router.get("/", (req, res) => {
-  Comment.find({}, (err, comments) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).json(comments);
-    }
-  });
-});
-
-// Get a comment by id
-router.get("/:id", (req, res) => {
-  Comment.findById(req.params.id, (err, comment) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).json(comment);
-    }
-  });
-});
-
-// Add a comment
-router.post("/", (req, res) => {
-  const comment = new Comment(req.body);
-  comment.save((err, comment) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(201).json(comment);
-    }
-  });
-});
-
-// Update an existing comment
-router.put("/:id", (req, res) => {
-  Comment.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true },
-    (err, comment) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.status(200).json(comment);
-      }
-    }
-  );
-});
-
-// Delete a comment
-router.delete("/:id", (req, res) => {
-  Comment.findByIdAndRemove(req.params.id, (err, comment) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).json(comment);
-    }
-  });
-});
-
-// Export Module
 module.exports = router;
